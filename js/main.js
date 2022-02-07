1d2623b4ab5b171b06da249bbb1e952d91094043
pokiza1228 <baxromovapokiza1@gmail.com>
2022-02-07 14:32:19 +0500

    15

--- js/main.js
+++ js/main.js
@@ -3,6 +3,6 @@ const nav = document.querySelector(".header__nav");
 const intro = document.querySelector(".intro__content");
 btn.addEventListener("click", function() {
     btn.classList.toggle("header__btn--opened");
-    nav.classList.toggle("header__nav");
+    nav.classList.toggle("header__nav--opened");
     intro.classList.toggle("header__btn--opened");
 })
